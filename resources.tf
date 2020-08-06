52d06931c0dc8eb13752ea23e8695ca293a398c4
bmwitcher <65639961+bmwitcher@users.noreply.github.com>
2020-08-05 22:37:09 -0400

    Add files via upload

---
+++ resources.tf
@@ -0,0 +1,45 @@
+resource "aws_iam_user" "newemployees" {
+  count = length(var.iam_names)
+  name          = element(var.iam_names,count.index)
+  path          = "/system/"
+  force_destroy = true
+}
+
+resource "aws_iam_user_login_profile" "example" {
+  count = 3
+  user   = element(var.iam_names,count.index)
+  pgp_key = "keybase:some_person_that_exists"
+  password_reset_required = true
+}
+
+
+resource "aws_iam_account_password_policy" "strict" {
+  minimum_password_length        = 8
+  require_lowercase_characters   = true
+  require_numbers                = true
+  require_uppercase_characters   = true
+  require_symbols                = false
+  allow_users_to_change_password = true
+}
+
+resource "aws_iam_user_policy" "newemployees_policy" {
+  count = 5
+  user   = element(var.iam_names,count.index)
+  name = "testpolicy"
+
+
+  policy = <<EOF
+{
+ "Version": "2012-10-17",
+ "Statement": [
+   {
+     "Action": [
+       "ec2:Describe*"
+     ],
+     "Effect": "Allow",
+     "Resource": "*"
+   }
+ ]
+}
+EOF
+}
